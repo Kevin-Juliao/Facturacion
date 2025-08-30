80208c5444833cfb9a1c90d3becd1d19b5cf66fc
kevin <kjuliao@unicesar.edu.co>
2025-08-30 11:41:45 -0500

    Third commit

--- src/main/java/co/edu/unicesar/facturacion/Launcher.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unicesar.facturacion;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
